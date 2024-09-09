7b3cd7be77fafb4686d87d5bd733f4a513fd9bda
wkoju84 <wkoju84@gmail.com>
2024-09-09 16:26:52 -0300

    Adiciona buscarPorId em ClienteService

--- src/main/java/com/bancaria/transacao/services/ClienteService.java
+++ src/main/java/com/bancaria/transacao/services/ClienteService.java
@@ -1,12 +1,16 @@
 package com.bancaria.transacao.services;
 
 import com.bancaria.transacao.dtos.ClienteDTO;
+import com.bancaria.transacao.dtos.EmpresaDTO;
 import com.bancaria.transacao.entities.Cliente;
+import com.bancaria.transacao.entities.Empresa;
 import com.bancaria.transacao.repositories.ClienteRepository;
+import com.bancaria.transacao.services.exceptions.EntidadeNaoEncontradaException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,5 +26,14 @@ public class ClienteService {
     public List<ClienteDTO> buscarTodos(){
         List<Cliente> list = clienteRepository.findAll();
         return list.stream().map(ClienteDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public ClienteDTO buscarPorId(Long id){
+        Optional<Cliente> objeto = clienteRepository.findById(id);
+        Cliente entidade = objeto.orElseThrow(() -> new EntidadeNaoEncontradaException(
+                "Registro " + id + " não encontrado em sua base de dados!"
+        ));
+        return new ClienteDTO(entidade);
     }
 }
