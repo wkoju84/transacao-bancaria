5567abaa694bcb4042edcf2592f49c60ca816f62
wkoju84 <wkoju84@gmail.com>
2024-09-09 17:02:33 -0300

    Adiciona buscarPorId em TransacaoService

--- src/main/java/com/bancaria/transacao/services/TransacaoService.java
+++ src/main/java/com/bancaria/transacao/services/TransacaoService.java
@@ -1,12 +1,16 @@
 package com.bancaria.transacao.services;
 
+import com.bancaria.transacao.dtos.EmpresaDTO;
 import com.bancaria.transacao.dtos.TransacaoDTO;
+import com.bancaria.transacao.entities.Empresa;
 import com.bancaria.transacao.entities.Transacao;
 import com.bancaria.transacao.repositories.TransacaoRepository;
+import com.bancaria.transacao.services.exceptions.EntidadeNaoEncontradaException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,5 +26,14 @@ public class TransacaoService {
     public List<TransacaoDTO> buscarTodos(){
         List<Transacao> list = transacaoRepository.findAll();
         return list.stream().map(TransacaoDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public TransacaoDTO buscarPorId(Long id){
+        Optional<Transacao> objeto = transacaoRepository.findById(id);
+        Transacao entidade = objeto.orElseThrow(() -> new EntidadeNaoEncontradaException(
+                "Registro " + id + " não encontrado em sua base de dados!"
+        ));
+        return new TransacaoDTO(entidade);
     }
 }
