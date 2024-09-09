4c29e2b0ecd3698c67c6a60c21f293ee9bffb98a
wkoju84 <wkoju84@gmail.com>
2024-09-09 12:15:43 -0300

    Adiciona classe Cliente em estities

---
+++ src/main/java/com/bancaria/transacao/entities/Cliente.java
@@ -0,0 +1,47 @@
+package com.bancaria.transacao.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nome, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
