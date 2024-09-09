b8c9e3470ac9ce78ab2fe1d3c5e8d15bb4bdb87e
wkoju84 <wkoju84@gmail.com>
2024-09-09 12:49:49 -0300

    Adiciona TransacaoRepository em repositories

--- src/main/java/com/bancaria/transacao/repositories/ClienteRepository.java
+++ src/main/java/com/bancaria/transacao/repositories/ClienteRepository.java
@@ -2,6 +2,8 @@ package com.bancaria.transacao.repositories;
 
 import com.bancaria.transacao.entities.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
--- src/main/java/com/bancaria/transacao/repositories/TransacaoRepository.java
+++ src/main/java/com/bancaria/transacao/repositories/TransacaoRepository.java
@@ -1,9 +1,9 @@
 package com.bancaria.transacao.repositories;
 
-import com.bancaria.transacao.entities.Empresa;
+import com.bancaria.transacao.entities.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
 }
